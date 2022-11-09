52b984eb69ed3b79263dce128bbbb5cfbf48514b
Bintang Nasution <sumbercodeku@gmail.com>
2022-11-09 21:02:23 +0700

    add contoh3

---
+++ contoh3.cpp
@@ -0,0 +1,13 @@
+#include <conio.h>
+#include <iostream.h>
+
+void cetakGaris(){
+	cout<<"-------------------- \n";
+}
+int main(){
+	cetakGaris();
+	cout<<"Contoh function void pada cetakGaris()\n";
+	cetakGaris();
+	getch();
+}
+
